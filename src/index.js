6b48a08a304f8a925ba72fb9d4bb9761281b2d75
emm-ser <106416@bk.ru>
2018-02-28 18:57:59 +0300

    additional_5/feature: 7 test missed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var openBrackets = [];
+    var map = new Map();
+
+
+    for (var i = 0; i < bracketsConfig.length; i++) {
+
+        openBrackets.push(bracketsConfig[i][0]);
+        map.set(bracketsConfig[i][0], bracketsConfig[i][1]);
+
+    }
+
+    var pattern = [];
+
+    for (var i = 0; i < str.length; i++) {
+        if (openBrackets.indexOf(str[i]) >= 0) {
+            pattern.push(str[i]);
+        } else if (str[i] != map.get(pattern.pop())) {
+            return false;
+        }
+    }
+
+    return pattern.length > 0 ? false : true;
 }
